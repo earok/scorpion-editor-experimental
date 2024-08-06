416a403ca9c30cc6218c388b2b0a24e6089fb29a
Erik Hogan <earok3d@gmail.com>
2024-08-06 18:03:57 +1200

    Latest fixes

--- Editor/Scorpion Editor - 2024.1_Data/StreamingAssets/plugins/compile plugins.bat
+++ Editor/Scorpion Editor - 2024.1_Data/StreamingAssets/plugins/compile plugins.bat
@@ -2,4 +2,7 @@ cd xgm
 ..\vasmm68k_mot -Fbin -nosym -o plugin_xgm.bin plugin_xgm.asm
 cd ..\amiga_boot_sector
 ..\vasmm68k_mot -Fbin -nosym -o bootsector.bin bootsector.asm
+cd ..\photon_loader
+..\vasmm68k_mot -Fbin -nosym -o MFMloader.bin MFMloader.asm
 pause
+
